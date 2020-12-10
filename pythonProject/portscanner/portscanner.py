58fc41d0955012789d1673d7ad6196690a19f05a
joshiupendra1995 <joshiupendra34@gmail.com>
2020-12-10 08:51:22 -0500

    portscanner modified

--- pythonProject/portscanner/portscanner.py
+++ pythonProject/portscanner/portscanner.py
@@ -1,6 +1,6 @@
 import socket
 from IPy import IP
-print("\033[1;31;40m   \n")  # sets console font to white
+
 print("\033[1;36;40m   \n")  # sets below text to cyan color
 print(
     "**     **       **    ********   *******  ********  ********     ******   ******     ***    **    ** **    ** ******** ********")
@@ -18,6 +18,13 @@ print(
     "*******   ******     **         *******  **     **    **        ******   ******  **     ** **    ** **    ** ******** **     **")
 
 
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n'+f'[ - 0 Scanning Target] {target}')
+    for port in range(port_start_range, port_end_range):
+        scan_port(converted_ip, port)
+
+
 def check_ip(ip):
     try:
         IP(ip)
@@ -31,23 +38,20 @@ def scan_port(ip, port):
         sock = socket.socket()
         sock.settimeout(0.5)
         sock.connect((ip, port))
-        print("\033[1;31;40m   \n")  # sets color to red
-        print(f'[+] Port {p} Is Open')
-        print("\033[1;37;40m   \n")  # sets color to white
+        print("\033[1;36;40m   \n")
+        print('\n'+f'[+] Port {port} Is Open')
+        print("\033[1;37;40m   \n")  # sets console font to white
     except:
-        pass
+        print("\033[1;31;40m   \n")  # sets color to red
+        print('\n'+f'[-] Port {port} Is Closed')
 
 
 print("\033[1;32;40m   \n")
 ipaddress = input('[+] Enter Target To Scan: ')
+port_start_range = int(input('[+] Enter The Port Start Range To Scan: '))
+port_end_range = int(input('[+]Enter The Port End Range To Scan: '))
 if ',' in ipaddress:
     for ip in ipaddress.split(','):
-        converted_ip = check_ip(ip)
-        print(f'[+] Scanning Port for ip {converted_ip}')
-        for p in range(75, 81):
-            scan_port(converted_ip, p)
+        scan(ip)
 else:
-    converted_ip = check_ip(ipaddress)
-    print(f'[+] Scanning Port for ip {converted_ip}')
-    for p in range(75, 81):
-        scan_port(converted_ip, p)
+    scan(ipaddress)
